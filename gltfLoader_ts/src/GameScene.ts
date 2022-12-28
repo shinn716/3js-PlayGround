6f0ddbf4c64f0ac4c802ff5651938e36d1494dc1
shinn716 <shinn716@livemail.tw>
2022-12-28 15:10:03 +0800

    Update project of the gltfloader_ts.

--- gltfLoader_ts/src/GameScene.ts
+++ gltfLoader_ts/src/GameScene.ts
@@ -1,11 +1,18 @@
 import * as THREE from 'three'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 export default class GameScene extends THREE.Scene
 {
 	private readonly camera: THREE.PerspectiveCamera
-    private group = new THREE.Group()
+    private obj = new THREE.Group
+    private group = new THREE.Group
     private rotSpeed: number = .00025
+    private mixer?: THREE.AnimationMixer
+    private prevTime = Date.now()
+    private animIndex: number = 0
+
+    private clips = []
+    private clipNames : string[] = []
 
 	constructor(camera: THREE.PerspectiveCamera)
 	{
@@ -15,17 +22,6 @@ export default class GameScene extends THREE.Scene
     
     async Initialize()
 	{
-        const loader = new GLTFLoader();
-        loader.load('assets/2CylinderEngine.glb', (loader) =>{
-            var obj = loader.scene
-            var size = .005
-            obj.position.z = 0
-            obj.scale.set(size, size, size)
-            this.group.add(obj)
-            this.add(this.group)
-        })
-
-
         // Light
         const ambientLight = new THREE.AmbientLight(0x666666);
         this.add(ambientLight);
@@ -33,12 +29,86 @@ export default class GameScene extends THREE.Scene
         const light = new THREE.DirectionalLight(0xFFFFFF, 1)
 		light.position.set(0, 4, 2)
         this.add(light)
+
+        const lightHelper = new THREE.DirectionalLightHelper(light)
+        this.add(lightHelper)
+            
+        const axesHelper = new THREE.AxesHelper(5); 
+        this.group.add(axesHelper)
     }
+
+    SetAnimationIndex(index: number){
+        this.animIndex = index
+    }
+
+    SetModelSize(size: number)
+    {
+        this.group.scale.set(size, size, size)
+    }
+
+    LoadGLTF(path : string)
+    {     
+        this.animIndex = 0
+        this.group.remove(this.obj)
+
+        const loader = new GLTFLoader();
+        loader.load(path, (loader) =>{
+            this.obj = loader.scene
+
+            var size = .01
+            this.obj.position.z = 0
+            this.obj.scale.set(size, size, size)
+            this.group.add(this.obj)
+            this.add(this.group)
+
+            console.log(loader.animations.length)
+
+            if(loader.animations.length==0)
+                return
+
+            console.log('==Animation clip list==')
+            for(let i=0; i<loader.animations.length; i++){
+                console.log(loader.animations[i].name)   
+                this.clipNames[i] = loader.animations[i].name
+            }
+
+            this.mixer = new THREE.AnimationMixer(this.obj)
+            this.mixer.clipAction(loader.animations[this.animIndex]).setDuration(1).play()
+            this.clips = loader.animations
+            
+        })
+    }
+
+    PlayAnimate()
+    {
+        console.log("Play " + this.animIndex + " " + this.clipNames[this.animIndex])
+        const clip = THREE.AnimationClip.findByName(this.clips, this.clipNames[this.animIndex] );
+        const action = this.mixer?.clipAction( clip );
+        action?.play();
+    }
+
+    // Test()
+    // {
+    //     console.log("hello")
+    //     const loader = new GLTFLoader()
+    //     loader.load('assets/Horse.glb', (gltf)=>{
+    //         let mesh = gltf.scene.children[0]
+    //         mesh.scale.set(.01, .01, .01)
+    //         this.add(mesh)
+    //         this.mixer = new THREE.AnimationMixer(mesh)
+    //         this.mixer.clipAction(gltf.animations[0]).setDuration(1).play()
+    //     })
+    // }
 
     Update()
 	{
 		// update
         this.group?.rotateY(Math.PI * this.rotSpeed)
+
+        // animation
+        const time = Date.now();
+        this.mixer?.update( ( time - this.prevTime ) * 0.001 );
+        this.prevTime = time;
 	}
 }
 
--- gltfLoader_ts/src/main.ts
+++ gltfLoader_ts/src/main.ts
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import Status from 'stats.js'
+import * as dat from 'dat.gui'
 import Scene from './GameScene'
 
 const width = window.innerWidth
@@ -7,9 +9,11 @@ const height = window.innerHeight
 
 // webgl init
 const renderer = new THREE.WebGLRenderer({
+    antialias:true,
     canvas: document.getElementById('app') as HTMLCanvasElement
 })
 renderer.setSize(width, height)
+renderer.setPixelRatio(window.devicePixelRatio)
 
 // camera
 const mainCamera = new THREE.PerspectiveCamera( 
@@ -20,22 +24,68 @@ const mainCamera = new THREE.PerspectiveCamera(
                                                 )
 
 const orbit = new OrbitControls(mainCamera, renderer.domElement)
+
 mainCamera.position.set(0, 2, 5)
 orbit.update();
 
+
 // scene
 const scene = new Scene(mainCamera)
+scene.background = new THREE.Color(0xbfe3dd)
 scene.Initialize()
 
 // debug
 console.log('threejs version: ' + THREE.REVISION);
 
+// fps counter
+var stats = new Status()
+stats.showPanel(1)
+document.body.appendChild( stats.dom );
+
+// GUI
+const gui = new dat.GUI();
+const options = {
+  backgroundColor: 0xbfe3dd,
+  size: 1,
+  model: 'Fox.glb',
+  animationIndex: 0,
+  play: () => {scene.PlayAnimate()}
+}
+gui.addColor(options, 'backgroundColor').onChange((value)=>{
+  scene.background = new THREE.Color(value)
+})
+
+gui.add(options, 'size', .1, 5).onChange((value)=>{
+  scene.SetModelSize(value)
+})
+
+// Create dropdowns by passing an array or object of named values
+gui.add( options, 'model', {  'Fox' : 'Fox.glb', 'Horse'  : 'Horse.glb', '2CylinderEngine': '2CylinderEngine.glb', 'Buggy': 'Buggy.glb'} ).onChange((value)=>{
+  scene.LoadGLTF('assets/' + options.model)
+  console.log(options.model)
+})
+
+gui.add(options, 'animationIndex').onChange((value)=>{
+  scene.SetAnimationIndex(value)
+})
+
+gui.add(options, 'play');
+
+// Init load first model
+scene.LoadGLTF('assets/Fox.glb')
+
+Loop()
+
 // loop
 function Loop()
 {
+  stats.begin();
   scene.Update()
   renderer.render(scene, mainCamera)
+	stats.end();
   requestAnimationFrame(Loop)
 }
 
-Loop()
+
+
+
